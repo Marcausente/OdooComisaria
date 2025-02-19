f52acde4c1cf36304e1c48aa49fa4b800351261f
Marc Fernández <marcausente@gmail.com>
2025-02-19 16:46:08 +0100

    First commit

---
+++ extra-addons/modulo_trabajo_marc/models/models.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class ModuloTrabajoMarc(models.Model):
+    _name = 'modulo_trabajo_marc.modulo_trabajo_marc'
+    _description = 'Modulo acerca de una comisaria'
+
+    name = fields.Char(string='Nombre', required=True)
+    value = fields.Integer(string='Valor')
+    description = fields.Text(string='Descripción')
+
+#     value2 = fields.Float(compute="_value_pc", store=True)
+#
+#     @api.depends('value')
+#     def _value_pc(self):
+#         for record in self:
+#             record.value2 = float(record.value) / 100
+
