f0e516cc8d735a2c320e3685b27f533c517948dc
Marc Fernández <marcausente@gmail.com>
2025-02-19 17:12:41 +0100

    Manifest

--- extra-addons/modulo_trabajo_marc/__manifest__.py
+++ extra-addons/modulo_trabajo_marc/__manifest__.py
@@ -1,34 +1,22 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "modulo_trabajo_marc",
-
-    'summary': "Short (1 phrase/line) summary of the module's purpose",
-
+    'name': "Gestión Policial",
+    'summary': "Sistema de gestión para comisarías de policía",
     'description': """
-Long description of module's purpose
+        Módulo para gestionar:
+        - Agentes de policía
+        - Comisarías
+        - Rangos y divisiones
+        - Armamento
     """,
-
-    'author': "My Company",
-    'website': "https://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
-
-    # any module necessary for this one to work correctly
-    'depends': ['base'],
-
-    # always loaded
+    'author': "Tu Nombre",
+    'category': 'Human Resources',
+    'version': '1.0',
+    'depends': ['base', 'mail'],
     'data': [
         'security/ir.model.access.csv',
         'views/views.xml',
-        'views/templates.xml',
     ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+    'application': True,
 }
 
